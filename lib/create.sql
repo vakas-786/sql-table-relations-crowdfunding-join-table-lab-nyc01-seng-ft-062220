c3c694be3e14f5604cf342fcf22570a9f371e17f
vakas-786 <vakas0959@gmail.com>
2020-07-02 18:36:10 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date  INTEGER, end_date INTEGER);
+
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount NUMERIC, user_id INTEGER, project_id INTEGER
+);
